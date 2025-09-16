5b3b22dfaf2dda572c64dfd1f302249f756723b8
ellasiyuwang <ellawang@usc.edu>
2025-09-16 09:34:22 -0700

    Initial commit

---
+++ OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Ella Wang on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
